e6e2fee0b8fc4edbe3a7401d471d8f97f12b6a97
Ziad Ayman <106850500+ziaddaymann@users.noreply.github.com>
2022-12-13 04:33:22 +0200

    Update delay.c

--- 3Modes_TivaC/HAL/DELAY/src/delay.c
+++ 3Modes_TivaC/HAL/DELAY/src/delay.c
@@ -1,7 +1,8 @@
 #include "delay.h"
 volatile int32_t TimeDelay;
 
-void delay_ms_Init (void){
+void delay_ms_Init (void)
+{
 NVIC_ST_CTRL_R = 0; // disable SysTick during setup
 NVIC_ST_RELOAD_R = 16000 - 1;// reload value
 NVIC_ST_CURRENT_R = 0; // any write to current clears it
@@ -10,11 +11,13 @@ NVIC_ST_CTRL_R = 0x04; // enable SysTick with core clock and interrupts
 // enable interrupts after all initialization is finished
 }
 
-void SysTick_Handler (void) { // SysTick interrupt service routine
+void SysTick_Handler (void)
+{ // SysTick interrupt service routine
 if (TimeDelay > 0) // Prevent it from being negative
 	TimeDelay --; // TimeDelay is a global volatile variable
 }
-void _delay_ms(uint32_t time ){ //nTime : specifies the delay time length
+void _delay_ms(uint32_t time )
+{ //nTime : specifies the delay time length
 NVIC_ST_CTRL_R |= 0x03; 
 TimeDelay= time ; // TimeDelay must be declared as volatile
 while(TimeDelay != 0); // Busy wait
